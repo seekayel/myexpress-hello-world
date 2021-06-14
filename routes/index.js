17e37f8a80c09982cdcf46527f4c2d9881f1c22d
Kam <seekayel@users.noreply.github.com>
2021-06-14 07:44:42 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'hmmm cat' });
+  res.render('index', { title: 'meow cat' });
 });
 
 module.exports = router;
